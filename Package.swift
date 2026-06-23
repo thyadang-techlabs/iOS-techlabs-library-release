043581014b1a397f1511ce111d798c3b4e459d42
Techlabs-jeonhaedong <119987200+Techlabs-jeonhaedong@users.noreply.github.com>
2026-06-23 11:04:26 +0900

    chore: release 2.1.2

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ios-techlabs-library",
-            url: "https://github.com/thyadang-techlabs/iOS-techlabs-library-release/releases/download/2.1.1/ios_techlabs_library.xcframework.zip",
-            checksum: "acd52b5623717332a0c6ba8f981f4a9d6809615c6fcbfafc8bd2506101c20e6d"
+            url: "https://github.com/thyadang-techlabs/iOS-techlabs-library-release/releases/download/2.1.2/ios_techlabs_library.xcframework.zip",
+            checksum: "e288b3d6b06ffb9d0f7fe0f6d8af5fed6565d72b9421533f8e75b4150922ce0a"
         ),
     ]
 )
